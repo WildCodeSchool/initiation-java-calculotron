bb75a1277fd48ceb6b6ab159c1215da5569816e5
bastienwcs <email>
2019-08-27 16:53:36 +0200

    simplify repositories

--- calculotron.sh
+++ calculotron.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 clear
-javac -classpath out:lib/* -d out Main.java 2> log/error.txt
-if [[ $(< log/error.txt) != "" ]]; then
-    cat log/error.txt
+javac -cp :lib/* Main.java 2> error.log
+if [[ $(< error.log) != "" ]]; then
+    cat error.log
 else
-    java -classpath out:lib/* --module-path lib --add-modules javafx.controls,javafx.fxml Main &
+    java -cp :lib/* --module-path lib --add-modules javafx.controls Main &
 fi
